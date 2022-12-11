42f1e8722c703117a748e9174396a40613e854df
Manny Apsel <manny-apsel@hotmail.com>
2022-12-11 20:10:20 +0100

    remove annyoing warning for ex 83

--- Program.cs
+++ Program.cs
@@ -4,19 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var a = RemoveDuplicates.DeleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))));
-
-            var tf = true;
-
-            while (tf){
-                System.Console.WriteLine(a.val);
-                if (a.next != null){
-                    a = a.next;
-                }
-                else {
-                    tf = false;
-                }
-            }
+            
         }
     }
 }
